1c334d1c69d777e29e34629c7eaa8ea56f06c026
hustle-dev <dlwoabsdk@gmail.com>
2021-09-01 15:15:28 +0900

    feat: survey js completed

--- src/js/surveyImg.js
+++ src/js/surveyImg.js
@@ -1,5 +1,6 @@
 $(function () {
   let cnt = 0;
+
   let leftPic = [
     ['dog1', '개'],
     ['city', '도시'],
@@ -12,32 +13,26 @@ $(function () {
   ];
 
   $('.survey__select').click(function () {
-    if (cnt === 3) {
-      console.log('끝');
-    } else {
-      $('.img1').attr('src', `../assets/images/${leftPic[cnt][0]}.jpg`);
-      $('.img1').attr('alt', `${leftPic[cnt][1]}`);
-      $('.img1>figcaption').text('hi');
-      $('.img2').attr('src', `../assets/images/${rightPic[cnt][0]}.jpg`);
-      $('.img2').attr('alt', `${rightPic[cnt][1]}`);
-      cnt++;
-    }
+    $(this).addClass('selected');
+    $(this).css('transform', 'translateY(-300px)');
+    $(this).css('opacity', '0');
+    $('.survey__select').not('.selected').css('transform', 'translateY(300px)');
+    $('.survey__select').not('.selected').css('opacity', '0');
+    setTimeout(function () {
+      if (cnt === 3) {
+        console.log('끝');
+      } else {
+        $('.img1').attr('src', `../assets/images/${leftPic[cnt][0]}.jpg`);
+        $('.img1').attr('alt', `${leftPic[cnt][1]}`);
+        $('.img1').next().text(`${leftPic[cnt][1]}`);
+        $('.img2').attr('src', `../assets/images/${rightPic[cnt][0]}.jpg`);
+        $('.img2').attr('alt', `${rightPic[cnt][1]}`);
+        $('.img2').next().text(`${rightPic[cnt][1]}`);
+        cnt++;
+      }
+      $('.survey__select').css('opacity', '1');
+      $('.survey__select').css('transform', 'translateY(0)');
+    }, 1000);
+    $(this).removeClass('selected');
   });
-
-  // $(this).addClass('selected');
-  // $(this).css('transform', 'translateY(-300px)');
-  // $(this).css('opacity', '-1');
-  // $('.survey__select').not('.selected').css('transform', 'translateY(300px)');
-  // $('.survey__select').not('.selected').css('opacity', '-1');
-
-  // setTimeout(function () {
-  //   console.log('시간이후');
-
-  //   alert('실험');
-  // }, 1000);
-  // $(this).css('opacity', '0');
-  // $('.survey__select')
-  //   .not('.selected')
-  //   .css('transform', 'translateY(-300px)');
-  // $('.survey__select').not('.selected').css('opacity', '0');
 });
