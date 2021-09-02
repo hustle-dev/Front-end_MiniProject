504f63f1164f5f670d1c731be5722cbe76f40454
hustle-dev <dlwoabsdk@gmail.com>
2021-09-02 16:05:43 +0900

    feat: delete not using files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './sass/index.scss';
 import './js/header.js';
 import './js/lazyLoading';
 import './js/setThumbnail';
-import './js/setHeart';
 import './js/gallery.js';
 import './js/greeting.js';
 import './js/modal.js';
