670a6c3b10bb6b86f32b2eb72f0bd1dfe654debd
kheeyaa <kheeyaa@icloud.com>
2021-09-03 10:54:42 +0900

    style: Remove console.log

--- src/js/uploadButton.js
+++ src/js/uploadButton.js
@@ -1,11 +1,9 @@
 document.addEventListener('keydown', function (e) {
   const keyCode = e.keyCode;
-  console.log('pushed key ' + e.key);
   let focus = document.activeElement.className;
 
   if (keyCode == 13) {
     // Enter key
-    console;
     if (focus === 'register__item--upload') {
       $('#File').trigger('click');
     }
